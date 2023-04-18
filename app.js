c296e28433033bfb51057a57a4f0e856bc508193
abdullahbest123 <abdullahbest19@gmail.com>
2023-04-18 07:10:24 +0500

    new

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const path = require('path');
 const app = express();
 const connectDB = require('./db/connect')
 const jobsRouter = require('./routes/jobs')
 const authRouter = require('./routes/auth')
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const expressratelimiter = require('express-rate-limit');
+const cors = require('cors')
 const authicateUser = require('./middleware/authentication');
-
 // error handlers
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+app.use(expressratelimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
 
 // routes
 app.use('/api/v1/jobs', authicateUser, jobsRouter)
