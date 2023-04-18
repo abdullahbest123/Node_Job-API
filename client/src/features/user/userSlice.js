d6705a4092967e9c362d19a395cff1bff4d79158
abdullahbest123 <abdullahbest19@gmail.com>
2023-04-18 13:00:02 +0500

    prod

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,16 +1,18 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-import {
-  addUserToLocalStorage,
-  getUserFromLocalStorage,
-  removeUserFromLocalStorage,
-} from '../../utils/localStorage';
-import {
-  loginUserThunk,
-  registerUserThunk,
-  updateUserThunk,
-  clearStoreThunk,
-} from './userThunk';
+import
+  {
+    addUserToLocalStorage,
+    getUserFromLocalStorage,
+    removeUserFromLocalStorage,
+  } from '../../utils/localStorage';
+import
+  {
+    loginUserThunk,
+    registerUserThunk,
+    updateUserThunk,
+    clearStoreThunk,
+  } from './userThunk';
 
 const initialState = {
   isLoading: false,
@@ -20,21 +22,24 @@ const initialState = {
 
 export const registerUser = createAsyncThunk(
   'user/registerUser',
-  async (user, thunkAPI) => {
+  async (user, thunkAPI) =>
+  {
     return registerUserThunk('/auth/register', user, thunkAPI);
   }
 );
 
 export const loginUser = createAsyncThunk(
   'user/loginUser',
-  async (user, thunkAPI) => {
+  async (user, thunkAPI) =>
+  {
     return loginUserThunk('/auth/login', user, thunkAPI);
   }
 );
 
 export const updateUser = createAsyncThunk(
   'user/updateUser',
-  async (user, thunkAPI) => {
+  async (user, thunkAPI) =>
+  {
     return updateUserThunk('/auth/updateUser', user, thunkAPI);
   }
 );
@@ -43,52 +48,62 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    toggleSidebar: (state) => {
+    toggleSidebar: (state) =>
+    {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
-    logoutUser: (state, { payload }) => {
+    logoutUser: (state, { payload }) =>
+    {
       state.user = null;
       state.isSidebarOpen = false;
       removeUserFromLocalStorage();
-      if (payload) {
+      if (payload)
+      {
         toast.success(payload);
       }
     },
   },
   extraReducers: {
-    [registerUser.pending]: (state) => {
+    [registerUser.pending]: (state) =>
+    {
       state.isLoading = true;
     },
-    [registerUser.fulfilled]: (state, { payload }) => {
+    [registerUser.fulfilled]: (state, { payload }) =>
+    {
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
       addUserToLocalStorage(user);
       toast.success(`Hello There ${user.name}`);
     },
-    [registerUser.rejected]: (state, { payload }) => {
+    [registerUser.rejected]: (state, { payload }) =>
+    {
       state.isLoading = false;
       toast.error(payload);
     },
-    [loginUser.pending]: (state) => {
+    [loginUser.pending]: (state) =>
+    {
       state.isLoading = true;
     },
-    [loginUser.fulfilled]: (state, { payload }) => {
+    [loginUser.fulfilled]: (state, { payload }) =>
+    {
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
       addUserToLocalStorage(user);
-
       toast.success(`Welcome Back ${user.name}`);
     },
-    [loginUser.rejected]: (state, { payload }) => {
+    [loginUser.rejected]: (state, { payload }) =>
+    {
       state.isLoading = false;
       toast.error(payload);
     },
-    [updateUser.pending]: (state) => {
+    [updateUser.pending]: (state) =>
+    {
       state.isLoading = true;
     },
-    [updateUser.fulfilled]: (state, { payload }) => {
+    [updateUser.fulfilled]: (state, { payload }) =>
+    {
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
@@ -96,11 +111,13 @@ const userSlice = createSlice({
 
       toast.success(`User Updated!`);
     },
-    [updateUser.rejected]: (state, { payload }) => {
+    [updateUser.rejected]: (state, { payload }) =>
+    {
       state.isLoading = false;
       toast.error(payload);
     },
-    [clearStore.rejected]: () => {
+    [clearStore.rejected]: () =>
+    {
       toast.error('There was an error..');
     },
   },
--- controllers/auth.js
+++ controllers/auth.js
@@ -24,19 +24,60 @@ const login = async function (req, res)
         throw new UnauthenticatedError('Invalid Credentials')
     }
     const token = user.JwtVerify();
-    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+    res.status(StatusCodes.OK).json({
+        user: {
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name,
+            token,
+        },
+    });
 
 }
+const updateUser = async (req, res) =>
+{
+    const { email, name, lastName, location } = req.body;
+    if (!email || !name || !lastName || !location)
+    {
+        throw new BadRequest('Please provide all values');
+    }
+    const user = await User.findOne({ _id: req.user.userId });
 
+    user.email = email;
+    user.name = name;
+    user.lastName = lastName;
+    user.location = location;
+
+    await user.save();
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({
+        user: {
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name,
+            token,
+        },
+    });
+};
 const register = async function (req, res)
 {
     const user = await UserScheme.create({ ...req.body })
     const token = user.JwtVerify();
-    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
-
+    res.status(StatusCodes.OK).json({
+        user: {
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name,
+            token,
+        },
+    });
 }
 
 module.exports = {
     login,
-    register
+    register,
+    updateUser,
 }
